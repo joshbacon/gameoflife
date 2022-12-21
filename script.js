efb9d6a2dca4bac889f5b7f7b8f58ce7d84b152f
joshbacon <jdbacon@mta.ca>
2022-12-21 16:34:46 -0400

    fixed border issues and cleanedup

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let running = false;
-let size = 20;
+let size = 120;
 
 function trigger() {
     if (this.className.includes('dead')){
@@ -16,29 +16,36 @@ function iterate() {
         let cell = cells[c];
         let neighbours = 0;
         // check above
-        if (c-size >= 0 && cells[c-size].className.includes('alive')) 
+        if (c-size >= 0 && cells[c-size].className.includes('alive'))
             neighbours++;
         // check below
         if (c+size < cells.length && cells[c+size].className.includes('alive'))
             neighbours++;
-        // check left
-        if (c-1 >= 0 && cells[c-1].className.includes('alive'))
-            neighbours++;
-        // check right
-        if (c+1 < cells.length && cells[c+1].className.includes('alive'))
-            neighbours++;
-        // check top left
-        if (c-size+1 >= 0 && cells[c-size+1].className.includes('alive'))
-            neighbours++;
-        // check top right
-        if (c-size-1 >= 0 && cells[c-size-1].className.includes('alive'))
-            neighbours++;
-        // check bottom left
-        if (c+size-1 < cells.length && cells[c+size-1].className.includes('alive'))
-            neighbours++;
-        // check bottom right
-        if (c+size+1 < cells.length && cells[c+size+1].className.includes('alive'))
-            neighbours++;
+        // Only check left neighbours if not on left border
+        if (c % size !== 0) {
+            // check top left
+            if (c - size - 1 >= 0 && cells[c-size-1].className.includes('alive'))
+                neighbours++;
+            // check left
+            if (c - 1 >= 0 && cells[c-1].className.includes('alive'))
+                neighbours++;
+            // check bottom left
+            if (c + size - 1 < cells.length && cells[c+size-1].className.includes('alive'))
+                neighbours++;
+        }
+        // Only check right neighbours if not on right border
+        if ((c + 1) % size !== 0) {
+            // check right
+            if (c + 1 < cells.length && cells[c+1].className.includes('alive'))
+                neighbours++;
+            // check top right
+            if (c - size + 1 >= 0 && cells[c-size+1].className.includes('alive'))
+                neighbours++;
+            // check bottom right
+            if (c + size + 1 < cells.length && cells[c+size+1].className.includes('alive'))
+                neighbours++;
+        }
+
         // update cell status
         let status = 'cell dead';
         if (cell.className.includes('alive')) {
@@ -52,15 +59,22 @@ function iterate() {
         cells[c].className = statuses[c];
     }
 
-    if (running) {
+    if (running && statuses.includes('cell alive')) {
         setTimeout(() => {
             iterate();
         }, 500)
     }
 }
 
+function togglePause() {
+    running = !running;
+    if (running) iterate();
+}
+
 function stop() {
     running = false;
+    document.getElementById('start').setAttribute('style', 'display: grid;');
+    document.getElementById('pause').setAttribute('style', 'display: none;');
     let cells = document.getElementById('world').children;
     for (let c = 0; c < cells.length; c++) {
         cells[c].className = 'cell dead';
@@ -68,6 +82,10 @@ function stop() {
 }
 
 $(document).ready(function() {
+    document.getElementById('world').setAttribute('style',
+        `width: ${14.8*size}px;
+        grid-template-columns: repeat(${size}, 1fr);`
+    );
     let numCells = size**2;
     let world = document.getElementById('world');
     for (let c = 0; c < numCells; c++) {
@@ -78,9 +96,12 @@ $(document).ready(function() {
     }
 
     document.getElementById('start').addEventListener('click', () => {
+        document.getElementById('start').setAttribute('style', 'display: none;');
+        document.getElementById('pause').setAttribute('style', 'display: grid;');
         running = true;
         iterate();
     });
+    document.getElementById('pause').addEventListener('click', togglePause);
     document.getElementById('reset').addEventListener('click', stop);
 });
 
